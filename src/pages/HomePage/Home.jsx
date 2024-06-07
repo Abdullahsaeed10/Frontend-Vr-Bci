9b68ce507f9b3d54db9e8fd55385e708178ca118
Abdullah Saeed <96723171+Abdullahsaeed10@users.noreply.github.com>
2024-06-07 22:49:48 +0300

    home page

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,20 +1,21 @@
 import { Link } from "react-router-dom";
 import "./Home.css"; // Updated CSS for a more professional look
-// import Footer from '../../components/Footer'; // Ensure correct import path
 
 const HomePage = () => {
   return (
-    <div className="home-container">
+    <div className="home-container flex justify-center  lg:mt-40 mt-10 ">
       <header className="home-hero">
-        <h1>Welcome to Neurohike</h1>
-        <p>Delivering excellence</p>
-        <div className="hero-actions">
-          <Link to="/login" className="btn btn-secondary">
-            Login
-          </Link>
-          <Link to="/signup" className="btn btn-primary">
-            Register
-          </Link>
+        <div className="hero-content">
+          <h1>Welcome to NeurohikeVR</h1>
+          <p>Your path to recovery</p>
+          <div className="hero-actions">
+            <Link to="/login" className="btn btn-secondary">
+              Login
+            </Link>
+            <Link to="/signup" className="btn btn-primary">
+              Register
+            </Link>
+          </div>
         </div>
       </header>
       {/* Footer component */}
